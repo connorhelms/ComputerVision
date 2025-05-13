4f1f956f262eead97c7018564d0d704b86e3180a
Connor Helms <17yamos@gmail.com>
2025-05-13 11:08:42 -0400

    Week2

---
+++ Learning/month1/week2/week2.py
@@ -0,0 +1,15 @@
+import cv2
+import sys
+#1. Loading Images with OpenCV
+
+#"C:\Users\17yam\Desktop\Pfps""C:/Users/17yam/Desktop/Pfps/1.jpg"
+
+
+image_path = "C:/Users/17yam/Desktop/Pfps/mob_2.png"
+#Load in color to load in color image from {image_path}
+#Load in color to load in color image from {image_path}
+
+
+print(f"Attempting  )
+print(f"Attempting  )
+
